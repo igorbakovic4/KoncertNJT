0c18cf7dd3a9f1064e5ac358ee331f729ae6f0b0
BagZzi <igor.bakovic4@gmail.com>
2026-05-17 17:10:44 +0200

    Dodati repository interfejsi

--- koncert-app/src/main/java/rs/fon/koncert_app/repository/KartaRepository.java
+++ koncert-app/src/main/java/rs/fon/koncert_app/repository/KartaRepository.java
@@ -1,7 +1,9 @@
 package rs.fon.koncert_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rs.fon.koncert_app.entity.Karta;
 
+@Repository
 public interface KartaRepository extends JpaRepository<Karta, Long> {
 }
